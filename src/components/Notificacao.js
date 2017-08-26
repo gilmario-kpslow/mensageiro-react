7964a51d1e056ddfe2570ea85b56c4b3c97e5ed2
gilmario-kpslow <gilmariosoftware@gmail.com>
2017-08-25 23:20:10 -0300

    Iniciando

--- src/components/Notificacao.js
+++ src/components/Notificacao.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
-import {Button, Panel, ControlLabel, FormControl, FormGroup, Col, Row} from 'react-bootstrap'
+import { Button, Panel, ControlLabel, FormControl, FormGroup, Col, Row } from 'react-bootstrap'
 import PanelHeader from "./PanelHeader"
 import PanelFooter from "./PanelFooter"
-import musica from "../audio/alerta.mp3"
+import musica from "../audio/audio01.mp3"
+import ReactAudioPlayer from 'react-audio-player';
+
 
 export default class Login extends Component {
 
-    notificar(){
-        console.log(musica)
+constructor(){
+    super()
+    this.state ={}
+}
     
-        new Notification("Atenção",{
-            body:'Você tem um novo Chamado',
-             sound: musica
+
+
+    notificar() {
+        new Notification("Atenção", {
+            body: 'Você tem um novo Chamado'
         })
+        let player = (
+            <ReactAudioPlayer src={musica} autoPlay onEnded={()=>{ this.setState({player:undefined})} }/>
+        )
+        this.setState({player:player})
+        
     }
 
-    componentDidMount(){
+    componentDidMount() {
         Notification.requestPermission()
     }
+
+
 
     render() {
         return (
             <div className="container">
                 <Row>
                     <Col md={4} mdPush={4}>
-                        <Panel header={<PanelHeader titulo="Teste" icone="fa fa-user"/>} bsStyle="default" footer={
+                        <Panel header={<PanelHeader titulo="Teste" icone="fa fa-user" />} bsStyle="default" footer={
                             <PanelFooter>
                                 <Row>
                                     <Col md={6} xs={6}>
@@ -32,18 +45,18 @@ export default class Login extends Component {
                                     </Col>
                                 </Row>
                             </PanelFooter>
-                            }>
+                        }>
                             <Row>
                                 <Col md={12}>
-
+                                        {
+                                            this.state.player
+                                        }
                                 </Col>
                             </Row>
                         </Panel>
                     </Col>
                 </Row>
             </div>
-        );
+        )
     }
-
 }
-
